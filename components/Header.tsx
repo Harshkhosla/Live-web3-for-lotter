77a3405ebbf7d296f4dfe44b3b55de8a62bef104
harsh <harshkhosla9945@gmail.com>
2022-12-27 13:06:42 +0530

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { useEffect } from "react";
+import { useMoralis } from "react-moralis"
+
+export default function Header() {
+    const { enableWeb3, account, isWeb3Enabled, Moralis, deactivateWeb3, isWeb3EnableLoading} = useMoralis();
+    useEffect(() => {
+        if (isWeb3Enabled) return
+        if (typeof window !== "undefined") {
+            if (window.localStorage.getItem("connected")) {
+                enableWeb3()
+            }
+        }
+    }, [isWeb3Enabled])
+
+    useEffect(()=>{
+        Moralis.onAccountChanged((account)=>{
+            if(account=null){
+                window.localStorage.removeItem("connect")
+                deactivateWeb3()
+            }
+        })
+    },[])
+    return (<div>
+        {account ? <div>connected to {account}</div> :
+            <button onClick={async () => {
+                await enableWeb3()
+                if (typeof window !== "undefined") {
+                    { window.localStorage.setItem("connected", "injected") }
+                }
+            }}
+            disabled={isWeb3EnableLoading}>Connect</button>}
+    </div>)
+}
---
+++ components/Manualheader.tsx
@@ -0,0 +1,6 @@
+import { ConnectButton } from "web3uikit";
+export default function Manualheader(){
+    return(<div>
+        <ConnectButton moralisAuth={false}/>
+    </div>)
+}
